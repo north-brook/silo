a27ed31d1ec8ccf5cabe56c6283229950caeb00d
Bryce Bjork <brycedbjork@gmail.com>
2026-03-15 23:53:45 -0400

    Wrap app with CloudProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@
 import "./globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
+import { CloudProvider } from "../components/cloud";
 import { GitBar, GitBarProvider } from "../components/git-bar";
 import { NewWorkspaceProvider } from "../components/new-workspace";
 import { OpenProjectProvider } from "../components/open-project";
@@ -32,21 +33,23 @@ export default function RootLayout({
 					disableHoverableContent
 				>
 					<body className="flex flex-col h-screen overflow-hidden">
-						<GitBarProvider>
-							<ProjectsBarProvider>
-								<OpenProjectProvider>
-									<NewWorkspaceProvider>
-										<div className="flex flex-1 min-h-0">
-											<ProjectsBar />
-											<main className="flex-1 min-w-0 overflow-hidden flex flex-col">
-												{children}
-											</main>
-											<GitBar />
-										</div>
-									</NewWorkspaceProvider>
-								</OpenProjectProvider>
-							</ProjectsBarProvider>
-						</GitBarProvider>
+						<CloudProvider>
+							<GitBarProvider>
+								<ProjectsBarProvider>
+									<OpenProjectProvider>
+										<NewWorkspaceProvider>
+											<div className="flex flex-1 min-h-0">
+												<ProjectsBar />
+												<main className="flex-1 min-w-0 overflow-hidden flex flex-col">
+													{children}
+												</main>
+												<GitBar />
+											</div>
+										</NewWorkspaceProvider>
+									</OpenProjectProvider>
+								</ProjectsBarProvider>
+							</GitBarProvider>
+						</CloudProvider>
 						<Toaster />
 					</body>
 				</TooltipProvider>
